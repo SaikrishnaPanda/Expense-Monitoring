2e733cff412f0248a3474c3de84a9c332343e07d
SaikrishnaPanda <s.krishna.panda@gmail.com>
2023-04-06 17:52:31 +0530

    Lifting State Up used for Filtered Component

---
+++ src/components/Expenses/ExpenseFilter.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "./ExpensesFilter.css";
+
+const ExpensesFilter = (props) => {
+  const FilteredYearHandler = (event) => {
+    // console.log(event.target.value);
+    const selectedYear = event.target.value;
+    props.getSelectedYear(selectedYear);
+  };
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control" onChange={FilteredYearHandler}>
+        <label>Filter by year</label>
+        <select>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,25 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
 const Expenses = (props) => {
+  const getSelectedYearHandler = (year) => {
+    console.log("inside Expense.js");
+    console.log(year);
+  };
   return (
-    <div className="expenses">
-      {props.items.map((e) => (
-        <ExpenseItem
-          key={e.id}
-          title={e.title}
-          amount={e.amount}
-          date={e.date}
-        ></ExpenseItem>
-      ))}
+    <div>
+      <ExpensesFilter getSelectedYear={getSelectedYearHandler} />
+      <Card className="expenses">
+        {props.items.map((e) => (
+          <ExpenseItem
+            key={e.id}
+            title={e.title}
+            amount={e.amount}
+            date={e.date}
+          ></ExpenseItem>
+        ))}
+      </Card>
     </div>
   );
 };
